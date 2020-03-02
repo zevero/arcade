b69118ee5a0fab7dbe263f00220c5c5e6e05f054
Zevero <zevero@users.noreply.github.com>
2020-03-02 15:03:21 +0100

    Initiale Dateien für das MakeCode Projekt

---
+++ README.md
@@ -0,0 +1,34 @@
+# arcade ![Build Status Abzeichen](https://github.com/zevero/arcade/workflows/MakeCode/badge.svg)
+
+
+
+## Diese Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* open https://arcade.makecode.com/
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* search for the URL of this repository and import
+
+## Diese Erweiterung bearbeiten
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* open https://arcade.makecode.com/
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* paste the repository URL and click import
+
+## Blockvorschau
+
+Dieses Bild zeigt den Blockcode vom letzten Commit im Master an.
+Die Aktualisierung dieses Bildes kann einige Minuten dauern.
+
+![Eine gerenderte Ansicht der Blöcke](https://github.com/zevero/arcade/raw/master/.makecode/blocks.png)
+
+## Supported targets
+
+* for PXT/arcade
+* for PXT/arcade
+(The metadata above is needed for package search.)
+
---
+++ main.ts
@@ -0,0 +1,80 @@
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy(effects.spray, 100)
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Erdbeere = sprites.createProjectileFromSprite(img`
+. . . . . . . 6 . . . . . . . . 
+. . . . . . 8 6 6 . . . 6 8 . . 
+. . . e e e 8 8 6 6 . 6 7 8 . . 
+. . e 2 2 2 2 e 8 6 6 7 6 . . . 
+. e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+. e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+e e 2 e 2 2 4 2 2 e e e c . . . 
+e e e e 2 e 2 2 e e e c . . . . 
+e e e 2 e e c e c c c . . . . . 
+. c c c c c c c . . . . . . . . 
+`, Ente, 100, 0)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.spray, 200)
+    info.changeLifeBy(-1)
+    Ente.startEffect(effects.fire, 1000)
+    Ente.setPosition(80, 60)
+    if (info.life() < 0) {
+        game.over(false)
+    }
+})
+let Fledermaus: Sprite = null
+let Erdbeere: Sprite = null
+let Ente: Sprite = null
+info.setLife(3)
+Ente = sprites.create(img`
+. . . . . . . . . . b 5 b . . . 
+. . . . . . . . . b 5 b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. . . . b b 5 d 1 f 5 5 d f . . 
+. . . . b 5 5 1 f f 5 d 4 c . . 
+. . . . b 5 5 d f b d d 4 4 . . 
+. b b b d 5 5 5 5 5 4 4 4 4 4 b 
+b d d d b b d 5 5 4 4 4 4 4 b . 
+b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+c b d c d 5 5 b 5 5 5 5 5 5 b . 
+. c d d c c b d 5 5 5 5 5 d b . 
+. . c b d d d d d 5 5 5 b b . . 
+. . . c c c c c c c c b b . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+Ente.setFlag(SpriteFlag.StayInScreen, true)
+controller.moveSprite(Ente, 200, 200)
+game.onUpdateInterval(Math.randomRange(500, 2000), function () {
+    Fledermaus = sprites.create(img`
+. . f f f . . . . . . . . f f f 
+. f f c c . . . . . . f c b b c 
+f f c c . . . . . . f c b b c . 
+f c f c . . . . . . f b c c c . 
+f f f c c . c c . f c b b c c . 
+f f c 3 c c 3 c c f b c b b c . 
+f f b 3 b c 3 b c f b c c b c . 
+. c 1 b b b 1 b c b b c c c . . 
+. c 1 b b b 1 b b c c c c . . . 
+c b b b b b b b b b c c . . . . 
+c b 1 f f 1 c b b b b f . . . . 
+f f 1 f f 1 f b b b b f c . . . 
+f f 2 2 2 2 f b b b b f c c . . 
+. f 2 2 2 2 b b b b c f . . . . 
+. . f b b b b b b c f . . . . . 
+. . . f f f f f f f . . . . . . 
+`, SpriteKind.Enemy)
+    Fledermaus.setPosition(180, Math.randomRange(0, 130))
+    Fledermaus.setVelocity(Math.randomRange(-100, -30), Math.randomRange(-10, 10))
+})
